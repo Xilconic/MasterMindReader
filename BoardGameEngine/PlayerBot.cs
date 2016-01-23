7f4a9f544adaa9a9250f84a1abdce653dc50a4a9
Xilconic <Xilconic@users.noreply.github.com>
2016-01-23 11:39:42 +0100

    Added Bot to play against.

---
+++ BoardGameEngine/PlayerBot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace BoardGameEngine
+{
+    /// <summary>
+    /// This class defines a bot opponent to play the game against.
+    /// </summary>
+    public class PlayerBot
+    {
+        private GameBoard board;
+        private int chosenHorizontalIndex, chosenVeritcalIndex;
+
+        /// <summary>
+        /// Instantiates a new instance of <see cref="PlayerBot"/> to play with a specified board.
+        /// </summary>
+        /// <param name="board">The game board to play with.</param>
+        public PlayerBot(GameBoard board)
+        {
+            this.board = board;
+
+            chosenHorizontalIndex = -1;
+            chosenVeritcalIndex = -1;
+        }
+
+        /// <summary>
+        /// Make the bot chose a location on the board that another player must deduce.
+        /// </summary>
+        /// <param name="random">The random number generator to be used.</param>
+        public void ChooseBoardElement(Random random)
+        {
+            chosenHorizontalIndex = random.Next(0, board.Size);
+            chosenVeritcalIndex = random.Next(0, board.Size);
+        }
+
+        /// <summary>
+        /// Ask the bot if the given board location has any feature in common with its chosen location.
+        /// </summary>
+        /// <param name="horizontalIndex">The row-index of the board location.</param>
+        /// <param name="verticalIndex">The column-index of the board location.</param>
+        /// <returns>True if any or all features match its chosen location; false if none of the features
+        /// match its chosen location.</returns>
+        public bool AskLocation(int horizontalIndex, int verticalIndex)
+        {
+            if (chosenHorizontalIndex == -1)
+            {
+                throw new InvalidOperationException("But must have chosen a location first (call 'PlayerBot.ChooseBoardElement()') before calling this method.");
+            }
+            if (horizontalIndex < 0 || horizontalIndex >= board.Size)
+            {
+                var message = string.Format("Argument must be in range [0, {0}], but was {1}.",
+                                            board.Size - 1, horizontalIndex);
+                throw new ArgumentOutOfRangeException("horizontalIndex", message);
+            }
+            if (verticalIndex < 0 || verticalIndex >= board.Size)
+            {
+                var message = string.Format("Argument must be in range [0, {0}], but was {1}.",
+                            board.Size - 1, verticalIndex);
+                throw new ArgumentOutOfRangeException("verticalIndex", message);
+            }
+
+            return chosenHorizontalIndex == horizontalIndex || chosenVeritcalIndex == verticalIndex ||
+                board.Elements[chosenHorizontalIndex, chosenVeritcalIndex].ElementValue == board.Elements[horizontalIndex, verticalIndex].ElementValue ||
+                board.Elements[chosenHorizontalIndex, chosenVeritcalIndex].SecondaryElementValue == board.Elements[horizontalIndex, verticalIndex].SecondaryElementValue;
+        }
+    }
+}
---
+++ MasterMindReader/BoardGameEngineTest/PlayerBotTest.cs
@@ -0,0 +1,204 @@
+﻿using BoardGameEngine;
+using NUnit.Framework;
+using System;
+
+namespace BoardGameEngineTest
+{
+    [TestFixture]
+    public class PlayerBotTest
+    {
+        [Test]
+        public void AskLocation_BeforeChosenBoardElement_ThrowInvalidOperationException()
+        {
+            // Setup
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+
+            // Call
+            TestDelegate call = () => bot.AskLocation(0, 0);
+
+            // Assert
+            var message = Assert.Throws<InvalidOperationException>(call).Message;
+            Assert.AreEqual("But must have chosen a location first (call 'PlayerBot.ChooseBoardElement()') before calling this method.", message);
+        }
+
+        [Test]
+        [TestCase(-20, 0)]
+        [TestCase(-1, 0)]
+        [TestCase(10, 0)]
+        [TestCase(20, 0)]
+        [TestCase(0, -30)]
+        [TestCase(0, -1)]
+        [TestCase(0, 10)]
+        [TestCase(0, 30)]
+        public void AskLocation_InvalidIndexes_ThrowArgumentOutOfRangeException(
+            int horizontal, int vertical)
+        {
+            // Setup
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(new Random());
+
+            // Call
+            TestDelegate call = () => bot.AskLocation(horizontal, vertical);
+
+            // Assert
+            Assert.Throws<ArgumentOutOfRangeException>(call);
+        }
+
+        [Test]
+        public void AskLocation_NoMatches_ReturnFalse()
+        {
+            // Setup
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(new Random(1));
+
+            // Call
+            var answer = bot.AskLocation(0, 0);
+
+            // Assert
+            Assert.IsFalse(answer);
+        }
+
+        [Test]
+        public void AskLocation_AtLocation_ReturnTrue()
+        {
+            // Setup
+            var seed = 2;
+            var inputRandom = new Random(seed);
+            var referenceRandom = new Random(seed);
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(inputRandom);
+
+            // Call
+            var locationIndex = referenceRandom.Next(0, board.Size);
+            var location2Index = referenceRandom.Next(0, board.Size);
+            var answer = bot.AskLocation(locationIndex, location2Index);
+
+            // Assert
+            Assert.IsTrue(answer);
+        }
+
+        [Test]
+        public void AskLocation_InSameColumn_ReturnTrue()
+        {
+            // Setup
+            var seed = 2;
+            var inputRandom = new Random(seed);
+            var referenceRandom = new Random(seed);
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(inputRandom);
+
+            // Call
+            var locationIndex = referenceRandom.Next(0, board.Size);
+            var location2Index = referenceRandom.Next(0, board.Size);
+            var answer = bot.AskLocation(locationIndex+1, location2Index);
+
+            // Assert
+            Assert.IsTrue(answer);
+        }
+
+        [Test]
+        public void AskLocation_InSameRow_ReturnTrue()
+        {
+            // Setup
+            var seed = 2;
+            var inputRandom = new Random(seed);
+            var referenceRandom = new Random(seed);
+            var board = new GameBoard();
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(inputRandom);
+
+            // Call
+            var locationIndex = referenceRandom.Next(0, board.Size);
+            var location2Index = referenceRandom.Next(0, board.Size);
+            var answer = bot.AskLocation(locationIndex, location2Index+1);
+
+            // Assert
+            Assert.IsTrue(answer);
+        }
+
+        [Test]
+        public void AskLocation_ElementWithSameElementValue_ReturnTrue()
+        {
+            // Setup
+            var seed = 2;
+            var inputRandom = new Random(seed);
+            var referenceRandom = new Random(seed);
+            var board = new GameBoard();
+
+            var locationIndex = referenceRandom.Next(0, board.Size);
+            var location2Index = referenceRandom.Next(0, board.Size);
+            var expectedChosenElement = board.Elements[locationIndex, location2Index];
+
+            int inputRowIndex = -1, intputColumnIndex = -1;
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    if (expectedChosenElement.ElementValue == board.Elements[i, j].ElementValue)
+                    {
+                        inputRowIndex = i;
+                        intputColumnIndex = j;
+                    }
+                }
+            }
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(inputRandom);
+
+            // Call
+
+            var answer = bot.AskLocation(inputRowIndex, intputColumnIndex);
+
+            // Assert
+            Assert.IsTrue(answer);
+        }
+
+        [Test]
+        public void AskLocation_ElementWithSameSecondaryElementValue_ReturnTrue()
+        {
+            // Setup
+            var seed = 2;
+            var inputRandom = new Random(seed);
+            var referenceRandom = new Random(seed);
+            var board = new GameBoard();
+
+            var locationIndex = referenceRandom.Next(0, board.Size);
+            var location2Index = referenceRandom.Next(0, board.Size);
+            var expectedChosenElement = board.Elements[locationIndex, location2Index];
+
+            int inputRowIndex = -1, intputColumnIndex = -1;
+            for (int i = 0; i < board.Size; i++)
+            {
+                for (int j = 0; j < board.Size; j++)
+                {
+                    if (expectedChosenElement.SecondaryElementValue == board.Elements[i, j].SecondaryElementValue)
+                    {
+                        inputRowIndex = i;
+                        intputColumnIndex = j;
+                    }
+                }
+            }
+
+            var bot = new PlayerBot(board);
+            bot.ChooseBoardElement(inputRandom);
+
+            // Call
+
+            var answer = bot.AskLocation(inputRowIndex, intputColumnIndex);
+
+            // Assert
+            Assert.IsTrue(answer);
+        }
+    }
+}
